caf7dc77867dec1ed95bf939c5e6016e76f8b749
Sathvik <dachepallisathvik@gmail.com>
2023-03-05 13:12:56 +0530

    Added Test Homepage

--- SinglePage/models.py
+++ SinglePage/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class NFTImages(models.Model):
+    title=models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/')
+    productId=models.AutoField(primary_key=True)
+    price = models.FloatField()
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
+class Invoice(models.Model):
+    status_choices=((-1,"Not Started"),(0,"Not confirmed"),(1,"Processing"),(2,"Paid"))
+
+    image=models.ForeignKey(NFTImages,on_delete=models.CASCADE)
+    invoiceId=models.AutoField(primary_key=True)
+    productId=models.IntegerField()
+    address=models.CharField(max_length=250)
+    buyer=models.CharField(max_length=100)
+    price = models.FloatField()
+    date = models.DateField()
+    status=models.IntegerField(choices=status_choices,default=-1)
+    def __str__(self):
+        return self.address
+
+class User(models.Model):
+    user_id=models.AutoField(primary_key=True)
+    username=models.CharField(max_length=100)
+    walletID=models.CharField(max_length=100)
+    def __str__(self):
+        return self.username
--- SinglePage/views.py
+++ SinglePage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import NFTImages,Invoice,User
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+
+def home(request):
+    products=NFTImages.objects.all()
+    return render(request,'index.html',context={'products':products})
